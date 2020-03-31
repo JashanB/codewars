a621a6356dffc494c82f2054685b2403baae78f3
JashanB <jashan_brar50@hotmail.com>
2020-03-30 20:50:44 -0400

    all tests passed

--- iqTest.js
+++ iqTest.js
@@ -11,7 +11,36 @@
 // Test.assertEquals(iqTest("2 4 7 8 10"),3);
 // Test.assertEquals(iqTest("1 2 2"), 1);
 
-
-function iqTest(numbers){
-  // ...
+function iqTest(numbers) {
+  const stringArray = numbers.split(' ');
+  const numArray = stringArray.filter(x => x !== " ")
+  const ifEven = numArray.filter(x => x % 2 === 0);  
+  if (ifEven.length === 1) {
+    const warpArray = numArray.map(function(num, index) {
+      if (num % 2 === 0) {
+        return num
+      } else {
+        return "X"
+      }
+    })
+    for (let i = 0; i < warpArray.length; i++) {
+      if (ifEven[0] === warpArray[i]) {
+        return i + 1;
+      }
+    }
+  } else {
+    const odd = numArray.filter(x => x % 2 !== 0);
+    const warpArray = numArray.map(function(num, index) {
+      if (num % 2 !== 0) {
+        return num
+      } else {
+        return "X"
+      }
+    })
+    for (let i = 0; i < warpArray.length; i++) {
+      if (odd[0] === warpArray[i]) {
+        return i + 1;
+      }
+    }
+  }
 }
