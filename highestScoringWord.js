914edda0b7b2c032e1ef814a188710798377cd7c
JashanB <jashan_brar50@hotmail.com>
2020-04-01 20:29:35 -0400

    1st commit

---
+++ highestScoringWord.js
@@ -0,0 +1,20 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+// Test.describe("Example tests",_=>{
+//   Test.assertEquals(high('man i need a taxi up to ubud'), 'taxi');
+//   Test.assertEquals(high('what time are we climbing up the volcano'), 'volcano'); 
+//   Test.assertEquals(high('take me to semynak'), 'semynak');  
+//   });
+
+
+function high(x){
+
+}
