4aa260db122af8e295062b3a1603dfb4b5ab717a
JashanB <jashan_brar50@hotmail.com>
2020-04-01 21:39:39 -0400

    all tests passed but n^3....learning

--- highestScoringWord.js
+++ highestScoringWord.js
@@ -16,5 +16,54 @@
 
 
 function high(x){
-
-}
+  let score = 0;
+  let result;
+  const alphabet = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+  };
+  const scoreObject = {};
+  const fullArray = x.split(" ");
+  for (let word of fullArray) {
+    scoreObject[word] = 0;
+    let singleWord = word.split('');
+    for (let character of singleWord) {
+      for (let alpha of Object.keys(alphabet)) {
+        if (character === alpha) {
+          scoreObject[word] += alphabet[alpha];
+        }
+      }
+    }
+  }
+  for (let word of Object.keys(scoreObject)) {
+    if (scoreObject[word] > score) {
+      score = scoreObject[word];
+      result = word;
+    }
+  }
+  return result;
+};
