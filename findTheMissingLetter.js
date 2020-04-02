cd5a1dbaba2e6d99efe959d1d9730d6266dc011e
JashanB <jashan_brar50@hotmail.com>
2020-04-01 23:09:56 -0400

    passed all tests - big O n

--- findTheMissingLetter.js
+++ findTheMissingLetter.js
@@ -12,3 +12,27 @@
 // ["a","b","c","d","f"] -> "e"
 // ["O","Q","R","S"] -> "P"
 // (Use the English alphabet with 26 letters!)
+function findMissingLetter(array) {
+  let result = '';
+  let isCapital = true;
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetArray = alphabet.split('');
+  if (array[0] === array[0].toLowerCase()) {
+    isCapital = false;
+  }
+  const lower = array.map(x => x.toLowerCase());
+  const startIndex = alphabetArray.indexOf(lower[0]);
+  const endIndex = alphabetArray.indexOf(lower[lower.length - 1]);
+  const slicedAlphabet = alphabetArray.slice(startIndex, endIndex);
+  for (let character of slicedAlphabet) {
+    let indexed = lower.indexOf(character);
+    if (indexed === -1) {
+      result = character;
+    }
+  }
+  if (isCapital) {
+    return result.toUpperCase();
+  } else {
+    return result;
+  }
+};
