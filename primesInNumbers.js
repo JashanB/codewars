2916ff4c9e31be9ee815b9cd03212934b6513643
JashanB <jashan_brar50@hotmail.com>
2020-04-04 20:26:18 -0400

    first commit

---
+++ primesInNumbers.js
@@ -0,0 +1,12 @@
+// Given a positive number n > 1 find the prime factor decomposition of n. The result will be a string with the following form :
+
+//  "(p1**n1)(p2**n2)...(pk**nk)"
+// with the p(i) in increasing order and n(i) empty if n(i) is 1.
+
+// Example: n = 86240 should return "(2**5)(5)(7**2)(11)"
+
+// Test.assertEquals(primeFactors(7775460),"(2**2)(3**3)(5)(7)(11**2)(17)")
+
+function primeFactors(n){
+  //your code here
+}
