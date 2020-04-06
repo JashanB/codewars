9229953f995aea8fc16a635433a97f489b8c6a34
JashanB <jashan_brar50@hotmail.com>
2020-04-06 12:41:24 -0400

    passes all tests

--- chunkyMonkeyFreeCodeCamp.js
+++ chunkyMonkeyFreeCodeCamp.js
@@ -9,13 +9,18 @@
 // chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
 // chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
 
-
 function chunkArrayInGroups(arr, size) {
-  const resultArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    for ()
-  }
-  return arr;
-}
+  let start = 0;
+  let newSize = size;
+  let resultArray = [];
+  do {
+    let array = arr.slice(start, newSize);
+    resultArray.push(array);
+    start += size;
+    newSize += size
+  } while (start < arr.length);
+  return resultArray;
+};
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2))
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
