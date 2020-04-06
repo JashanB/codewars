2a90f4324d370288aa30c6f9f76fd3c489ff2a1c
JashanB <jashan_brar50@hotmail.com>
2020-04-05 21:52:36 -0400

    adding in FCC algorithm practice

---
+++ chunkyMonkeyFreeCodeCamp.js
@@ -0,0 +1,21 @@
+// Basic Algorithm Scripting: Chunky Monkey
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+
+
+function chunkArrayInGroups(arr, size) {
+  const resultArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    for ()
+  }
+  return arr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
