76435b75383ab86026b4386fde5808552eea7401
JashanB <jashan_brar50@hotmail.com>
2020-04-06 20:00:45 -0400

    all tests passed but timing out

--- primesInNumbers.js
+++ primesInNumbers.js
@@ -7,6 +7,48 @@
 
 // Test.assertEquals(primeFactors(7775460),"(2**2)(3**3)(5)(7)(11**2)(17)")
 
-function primeFactors(n){
-  //your code here
-}
+function primeFactors(n) {
+  let number = n;
+  let resultArray = [];
+  let resultObject = {};
+  let isDivisable = true;
+  const returnFirstDivisor = function (num) {
+    for (let i = num; i > 1; i--) {
+      if (i !== num && num % i === 0) {
+        return [i, num / i];
+      };
+    };
+  };
+  do {
+    let newArray = returnFirstDivisor(number);
+    if (newArray === undefined) {
+      return `(${n})`;
+    }
+    resultArray = resultArray.map(function (array, index) {
+      if (array[0] === number) {
+        return [array[1]];
+      } else {
+        return array;
+      }
+    });
+    resultArray.push(newArray);
+    number = newArray[0];
+  } while (returnFirstDivisor(number) !== undefined);
+  for (let array of resultArray) {
+    for (let i = 0; i < array.length; i++) {
+      if (resultObject[array[i]]) {
+        resultObject[array[i]][1] += 1;
+      } else {
+        resultObject[array[i]] = [array[i], 1]
+      };
+    };
+  };
+  const result = Object.values(resultObject).map(function(array) {
+    if (array[1] > 1) {
+      return `(${array[0]}**${array[1]})`;
+    } else {
+      return `(${array[0]})`;
+    }
+  });
+  return result.join('');
+};
