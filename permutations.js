39a737baaf7c1ed81fdaaf3db1df9e123c2b2950
JashanB <jashan_brar50@hotmail.com>
2020-04-06 21:30:55 -0400

    first commit

---
+++ permutations.js
@@ -0,0 +1,21 @@
+// In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+
+// Examples:
+
+// permutations('a'); // ['a']
+// permutations('ab'); // ['ab', 'ba']
+// permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+// The order of the permutations doesn't matter.
+
+
+// describe('permutations', function() {
+//   it('examples from description', function() {
+//     Test.assertSimilar(permutations('a'), ['a']);
+//     Test.assertSimilar(permutations('ab').sort(), ['ab', 'ba'].sort());
+//     Test.assertSimilar(permutations('aabb').sort(), ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa'].sort());
+//   });
+// });
+
+function permutations(string) {
+  
+}
