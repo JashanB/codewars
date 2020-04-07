f69e9459cacfdd6eb09a620cfa60ecb01e0a5e0a
JashanB <jashan_brar50@hotmail.com>
2020-04-07 19:12:38 -0400

    first commit

---
+++ stringIncrementer.js
@@ -0,0 +1,29 @@
+// Your job is to write a function which increments a string, to create a new string.
+
+// If the string already ends with a number, the number should be incremented by 1.
+// If the string does not end with a number. the number 1 should be appended to the new string.
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
+
+// foo0042 -> foo0043
+
+// foo9 -> foo10
+
+// foo099 -> foo100
+
+// Attention: If the number has leading zeros the amount of digits should be considered.
+
+// Test.assertEquals(incrementString("foobar000"), "foobar001");
+// Test.assertEquals(incrementString("foo"), "foo1");
+// Test.assertEquals(incrementString("foobar001"), "foobar002");
+// Test.assertEquals(incrementString("foobar99"), "foobar100");
+// Test.assertEquals(incrementString("foobar099"), "foobar100");
+// Test.assertEquals(incrementString(""), "1");
+
+
+function incrementString (strng) {
+  // return incrementedString
+}
