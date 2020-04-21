47c18f888385bf2d531bae7943b892210b3016a6
JashanB <jashan_brar50@hotmail.com>
2020-04-21 16:06:27 -0400

    adding duplicates of nums if in range

--- rangeExtraction.js
+++ rangeExtraction.js
@@ -12,5 +12,13 @@
 // Test.assertEquals(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]), "-6,-3-1,3-5,7-11,14,15,17-20")
 
 function solution(list){
-  // TODO: complete solution 
- }
+  let duplicatesArr = [];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i + 1] === (list[i] + 1)) {
+      duplicatesArr.push(list[i])
+      duplicatesArr.push(list[i + 1])
+    } else {
+      duplicatesArr.push(list[i])
+    }
+  }
+}
