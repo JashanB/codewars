ee43ec5a575f57907205503d90c843894fb86d1e
JashanB <jashan_brar50@hotmail.com>
2020-11-08 13:54:22 -0500

    passes all tests

--- reverseString.js
+++ reverseString.js
@@ -1,5 +1,11 @@
 function solution(str){
-  
+  if (str.length === 0) { return '' };
+  const splitStr = str.split('');
+  const newArr = [];
+  for (let i = splitStr.length -1; i >= 0; i--) {
+    newArr.push(splitStr[i]);
+  }
+  return newArr.join('');
 }
 
 
