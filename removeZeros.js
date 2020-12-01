8f314fd0b8591985bd8e9e53d07e2a92c6b0c77a
JashanB <jashan_brar50@hotmail.com>
2020-12-01 10:00:01 -0500

    solved but has nulls in array

--- removeZeros.js
+++ removeZeros.js
@@ -1,15 +1,18 @@
 function removeZeros(array) {
-  // Sort "array" so that all elements with the value of zero are moved to the
-  // end of the array, while the other elements maintain order.
-  // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
-  // Zero elements also maintain order in which they occurred.
-  // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
-  
-  // Do not use any temporary arrays or objects. Additionally, you're not able
-  // to use any Array or Object prototype methods such as .shift(), .push(), etc
-  
-  // the correctly sorted array should be returned.
-  return array;
+  let numOfZeros = 0;
+  const newArray = array.map(function(item, index) {
+    if (item === 0 || item === "0") {
+      numOfZeros += 1;
+    }
+    if (item !== 0 && item !== "0") {
+      return item;
+    }
+  });
+  while (numOfZeros > 0) {
+    newArray.push(0)
+    numOfZeros -= 1;
+  }
+  return newArray;
 }
 
 // const input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14],
