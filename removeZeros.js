27a025e79c844441a1a3120ac3882f119c1317d7
JashanB <jashan_brar50@hotmail.com>
2020-12-03 13:45:39 -0500

    further changes

--- removeZeros.js
+++ removeZeros.js
@@ -1,19 +1,31 @@
+// function removeZeros(array) {
+//   let numOfZeros = 0;
+//   const newArray = array.filter(function(item) {
+//     if (item === 0 || item === "0") {
+//       numOfZeros += 1;
+//     }
+//     if (item !== 0 && item !== "0") {
+//       return item;
+//     }
+//   });
+//   while (numOfZeros > 0) {
+//     newArray.push(0)
+//     numOfZeros -= 1;
+//   }
+//   return newArray;
+// }
 function removeZeros(array) {
-  let numOfZeros = 0;
-  const newArray = array.filter(function(item) {
-    if (item === 0 || item === "0") {
-      numOfZeros += 1;
+  let zeroArray = [];
+  let newArray = [];
+  for (item of array) {
+    if (item === 0 || item === '0') {
+      zeroArray[zeroArray.length] = item;
+    } else {
+      newArray[newArray.lenth] = item;
     }
-    if (item !== 0 && item !== "0") {
-      return item;
-    }
-  });
-  while (numOfZeros > 0) {
-    newArray.push(0)
-    numOfZeros -= 1;
+
   }
   return newArray;
 }
-
 // const input = [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14],
 //     solution = [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0];
