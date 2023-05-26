49e7114dc2b32e4041f128c01f44289712beb4be
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-26 16:50:38 -0400

    added problem

---
+++ evenOdd.js
@@ -0,0 +1,29 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+/*
+Tests
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Sample tests",() => {
+  
+  it("2 is even", () => {
+    assert.strictEqual(evenOrOdd(2), "Even");
+  });
+  it("7 is odd", () => {
+    assert.strictEqual(evenOrOdd(7), "Odd");
+  });
+  it("-42 is even", () => {
+    assert.strictEqual(evenOrOdd(-42), "Even");
+  });
+  it("-7 is odd", () => {
+    assert.strictEqual(evenOrOdd(-7), "Odd");
+  });
+  it("0 is even", () => {
+    assert.strictEqual(evenOrOdd(0), "Even");
+  });
+});
+*/
+function evenOrOdd(number) {
+  
+}
