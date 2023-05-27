caf500607ea22fb4ca408f3cfabccbcff02a1942
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-27 12:21:54 -0400

    added challenge

---
+++ vowelCount.js
@@ -0,0 +1,18 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+
+const {assert} = require("chai");
+
+describe("Vowels Count Tests",function(){
+  it("should return 5 for 'abracadabra'",function(){
+    assert.strictEqual(getCount("abracadabra"), 5) ;
+  });
+});
+*/
+function getCount(str) {
+    return 0;
+  } 
