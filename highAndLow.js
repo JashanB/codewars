9c33ee5f1ce231002ccb334d7b77da2962d085fa
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-28 13:25:15 -0400

    added solution without sort function

--- highAndLow.js
+++ highAndLow.js
@@ -21,3 +21,19 @@ function highAndLow(numbers) {
     let string = sorted[sorted.length -1] + " " + sorted[0];
     return string
 }
+
+function highAndLow(numbers) {
+    let array = numbers.split(" ");
+    let low = parseFloat(array[0]);
+    let high = parseFloat(array[0]);
+    for (const value of array) {
+        if (parseFloat(value) < low) {
+            low = parseFloat(value);
+        }
+        if (parseFloat(value) > high) {
+            high = parseFloat(value);
+        }
+    }
+    let string = high + " " + low;
+    return string
+}
