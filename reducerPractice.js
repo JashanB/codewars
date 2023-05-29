6cfce3137c2bc629a69cb46414b921812be979f0
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-28 22:33:56 -0400

    practicing

---
+++ reducerPractice.js
@@ -0,0 +1,14 @@
+// const numbers = [1, 2, 3, 4, 5, 6];
+// //sum
+// //arr.reduce(callback(accumulator, currentValue), initialValue)
+
+// let sum = numbers.reduce((ac, cv) => ac + cv);
+
+// const numbers = [1800, 50, 300, 20, 100];
+
+// let subtract = numbers.reduce((ac, cv) => ac - cv)
+
+// const expenses = [1800, 2000, 3000, 5000, 500];
+// const salary = 15000;
+
+// let difference = expenses.reduce((ac, cv) => ac - cv, salary)
