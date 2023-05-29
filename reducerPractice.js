e679a99c4d41d7b15787bfbc1043ae8ddd4ce3e5
Jashan Brar <jashan_brar50@hotmail.com>
2023-05-28 23:22:04 -0400

    practicing

--- reducerPractice.js
+++ reducerPractice.js
@@ -12,3 +12,32 @@
 // const salary = 15000;
 
 // let difference = expenses.reduce((ac, cv) => ac - cv, salary)
+
+//duplicates
+
+// let ageGroup = [18, 21, 1, 1, 51, 18, 21, 5, 18, 7, 10];
+// // const returnAge = arr => {
+// //     let obj = {};
+// //     for (const age of ageGroup) {
+// //         if (!obj[age]) { obj[age] = 0}
+// //     }
+// //     return Object.keys(obj);
+// // }
+// // console.log(returnAge(ageGroup))
+// let uniqueAgeGroup = ageGroup.reduce(function (ac, cv) {
+//     if (ac.indexOf(cv) === -1) {
+//         ac.push(cv);
+//     }
+//     return ac;
+// }, []);
+// console.log(uniqueAgeGroup)
+
+
+let people = [
+    { name: "John", age: 21 },
+    { name: "Oliver", age: 55 },
+    { name: "Michael", age: 55 },
+    { name: "Dwight", age: 19 },
+    { name: "Oscar", age: 21 },
+    { name: "Kevin", age: 55 },
+  ];
